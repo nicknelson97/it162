d18a044cb7609a3e683d71500befe378d971cc6c
Dominick Nelson <60334994+nicknelson97@users.noreply.github.com>
2021-02-25 13:19:03 -0800

    Add files via upload

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
     <footer>
-      <p><small>&copy; 2021 by Dominick Nelson, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small> ~ <a href="contact.php">Contact</a></p>
+      <p><small>&copy; 2020 - <?php $year = date("Y"); echo $year; ?> by Dominick Nelson, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small> ~ <a href="contact.php">Contact</a></p>
     </footer>
      
   <script>
